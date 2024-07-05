887e69b95466a72d18d9079bfd2d489b365a5082
Alex Clapperton <alex.clapperton@codecomputerlove.com>
2024-07-05 09:15:28 +0100

    chore: CC-002 - 🔥 Remove app file to put back

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './components/app';
+import App from '';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
